59728d28d3a52ebc8153aa7140f2ac6dd111d5f9
Unknown <kabanos358@gmail.com>
2018-04-07 19:08:41 +0200

    Working models, done registers in admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Kraj)
+admin.site.register(Adres_zamieszkania)
+admin.site.register(Osoba)
+admin.site.register(Kryterium)
+admin.site.register(Wybor)
+admin.site.register(Kandydat)
+admin.site.register(Uprawniony)
+admin.site.register(Oddany_glos)
--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,50 @@
 from django.db import models
 
+class Kraj(models.Model):
+	idKraj = models.AutoField(primary_key=True)
+	nazwa = models.CharField(max_length=255)
+
 class Adres_zamieszkania(models.Model):
-	idAdresZamieszkania = models.IntegerField();
+	idAdresZamieszkania = models.AutoField(primary_key=True)
 	ulica = models.CharField(max_length=255)
 	numerBudynku = models.CharField(max_length=5)
 	numerLokalu = models.CharField(max_length=5)
 	kodPocztowy = models.CharField(max_length=6)
 	miejscowosc = models.CharField(max_length=255)
-	idKraj = models.IntegerField();
+	idKraj = models.ForeignKey(Kraj, on_delete=models.CASCADE)
+
+class Osoba(models.Model):
+	idOsoba = models.AutoField(primary_key=True)
+	imie = models.CharField(max_length=60)
+	nazwisko = models.CharField(max_length=60)
+	pesel = models.CharField(max_length=11)
+	idAdresZamieszkania = models.ForeignKey(Adres_zamieszkania, on_delete=models.CASCADE)
+
+class Kryterium(models.Model):
+	idKryterium = models.AutoField(primary_key=True)
+	dlugoscGlosowania = models.IntegerField()
+	iloscPoprartychKandydatow = models.IntegerField()
+
+class Wybor(models.Model):
+	idWybor = models.AutoField(primary_key=True)
+	nazwa = models.CharField(max_length=255)
+	dataRozpoczecia = models.DateTimeField('data rozpoczecia')
+	dataZakonczenia = models.DateTimeField('data zakonczenia')
+	opis = models.CharField(max_length=255)
+	idKryterium = models.ForeignKey(Kryterium, on_delete=models.CASCADE)
+
+class Kandydat(models.Model):
+	idKandydat = models.AutoField(primary_key=True)
+	idOsoba = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+	idWybor = models.ForeignKey(Wybor, on_delete=models.CASCADE)
+
+class Uprawniony(models.Model):
+	idUprawniony = models.AutoField(primary_key=True)
+	idOsoba = models.ForeignKey(Osoba, on_delete=models.CASCADE)
+	idWybor = models.ForeignKey(Wybor, on_delete=models.CASCADE)
+
+class Oddany_glos(models.Model):
+	idOddanyGlos = models.AutoField(primary_key=True)
+	dataOddaniaGlosu = models.DateTimeField('data oddania glosu')
+	idUprawniony = models.ForeignKey(Uprawniony, on_delete=models.CASCADE)
+	idWybor = models.ForeignKey(Wybor, on_delete=models.CASCADE)
