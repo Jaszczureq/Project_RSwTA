6c8e6f261d37d740385df958ce82d554c3176441
Unknown <kabanos358@gmail.com>
2018-04-07 02:57:06 +0200

    Up and running

--- Project_RSwTA/wsgi.py
+++ Project_RSwTA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Project_JG_MB project.
+WSGI config for Project_RSwTA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_JG_MB.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_RSwTA.settings')
 
 application = get_wsgi_application()
--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-Class Adres_zamieszkania(models.Model):
-	idAdresZamieszkania = models.IntegerField()
+class Adres_zamieszkania(models.Model):
+	idAdresZamieszkania = models.IntegerField();
 	ulica = models.CharField(max_length=255)
 	numerBudynku = models.CharField(max_length=5)
 	numerLokalu = models.CharField(max_length=5)
 	kodPocztowy = models.CharField(max_length=6)
 	miejscowosc = models.CharField(max_length=255)
-	idKraj = models.IntegerField()
+	idKraj = models.IntegerField();
