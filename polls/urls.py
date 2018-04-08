0e622cc38905bf37cf361cd4d2d2078235482edd
Unknown <kabanos358@gmail.com>
2018-04-08 02:21:48 +0200

    Some code formatting, listing Kraj for user

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:kraj_id>/', views.detail, name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Kraj
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	latest_kraj_list = Kraj.objects.order_by('-idKraj')
+	#template = loader.get_template('polls/index.html')
+	context = {
+		'latest_kraj_list': latest_kraj_list,
+	}
+	return render(request, 'polls/index.html', context)
+	
+def detail(request, kraj_id):
+    kraj = get_object_or_404(Kraj, pk=kraj_id)
+    return render(request, 'polls/detail.html', {'kraj': kraj})
