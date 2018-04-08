04ef63e1a56602cec1910aaa2d1d0f9aba0f2413
Unknown <kabanos358@gmail.com>
2018-04-08 21:29:29 +0200

    Improved database, views changed to generic

--- polls/models.py
+++ polls/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-
-
+ 
+ 
 class Kraj(models.Model):
     idKraj = models.AutoField(primary_key=True)
     nazwa = models.CharField(max_length=255)
-
-
+    def __str__(self):
+        return self.nazwa
+ 
+ 
 class Adres_zamieszkania(models.Model):
     idAdresZamieszkania = models.AutoField(primary_key=True)
     ulica = models.CharField(max_length=255)
@@ -14,22 +16,31 @@ class Adres_zamieszkania(models.Model):
     kodPocztowy = models.CharField(max_length=6)
     miejscowosc = models.CharField(max_length=255)
     idKraj = models.ForeignKey(Kraj, on_delete=models.CASCADE)
-
-
+    def __str__(self):
+        str = ""
+        str = self.ulica+' '+self.numerBudynku
+        if self.numerLokalu:
+            str += '/'+self.numerLokalu
+        str +=' '+self.kodPocztowy+' '+self.miejscowosc
+        return str
+ 
+ 
 class Osoba(models.Model):
     idOsoba = models.AutoField(primary_key=True)
     imie = models.CharField(max_length=60)
     nazwisko = models.CharField(max_length=60)
     pesel = models.CharField(max_length=11)
     idAdresZamieszkania = models.ForeignKey(Adres_zamieszkania, on_delete=models.CASCADE)
-
-
+    def __str__(self):
+        return self.imie+' '+self.nazwisko+' '+self.pesel
+ 
+ 
 class Kryterium(models.Model):
     idKryterium = models.AutoField(primary_key=True)
     dlugoscGlosowania = models.IntegerField()
     iloscPoprartychKandydatow = models.IntegerField()
-
-
+ 
+ 
 class Wybor(models.Model):
     idWybor = models.AutoField(primary_key=True)
     nazwa = models.CharField(max_length=255)
@@ -37,20 +48,23 @@ class Wybor(models.Model):
     dataZakonczenia = models.DateTimeField('data zakonczenia')
     opis = models.CharField(max_length=255)
     idKryterium = models.ForeignKey(Kryterium, on_delete=models.CASCADE)
-
-
+    def __str__(self):
+        return self.nazwa
+ 
+ 
 class Kandydat(models.Model):
     idKandydat = models.AutoField(primary_key=True)
     idOsoba = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     idWybor = models.ForeignKey(Wybor, on_delete=models.CASCADE)
-
-
+   
+ 
+ 
 class Uprawniony(models.Model):
     idUprawniony = models.AutoField(primary_key=True)
     idOsoba = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     idWybor = models.ForeignKey(Wybor, on_delete=models.CASCADE)
-
-
+ 
+ 
 class Oddany_glos(models.Model):
     idOddanyGlos = models.AutoField(primary_key=True)
     dataOddaniaGlosu = models.DateTimeField('data oddania glosu')
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-import re
+from django.views import generic
 
 from .models import *
 
-def index(request):
-	latest_kraj_list = Kraj.objects.order_by('-idKraj')
-	#template = loader.get_template('polls/index.html')
-	context = {
-		'latest_kraj_list': latest_kraj_list,
-	}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_kraj_list'
 	
-def detail(request, kraj_id):
-    kraj = get_object_or_404(Kraj, pk=kraj_id)
-    return render(request, 'polls/detail.html', {'kraj': kraj})
+	def get_queryset(self):
+		"""Return the last five published questions"""
+		return Kraj.objects.order_by('-idKraj')[:5]
+	
+class DetailView(generic.DetailView):
+	model = Kraj
+	template_name = 'polls/detail.html'
+	
+class ResultsView(generic.DetailView):
+	model = Kraj
+	template_name = 'polls/results.html'
 
 def vote(request, kraj_id):
 	kraj = get_object_or_404(Kraj, pk=kraj_id)
@@ -28,7 +32,3 @@ def vote(request, kraj_id):
 		selected_adres.votes += 1
 		selected_adres.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(kraj.id,)))
-	
-def results(request, kraj_id):
-	question = get_object_or_404(Kraj, pk=kraj_id)
-	return render(request, 'polls/results.html', {'question':question})
