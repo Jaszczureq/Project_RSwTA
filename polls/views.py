30b249e65d419cec0927eeb2bec175be07ee1bba
Unknown <kabanos358@gmail.com>
2018-04-08 15:59:21 +0200

    Voting in '/polls/'

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
+from django.http import Http404, HttpResponse
 
 from .models import Kraj
 
@@ -14,3 +14,10 @@ def index(request):
 def detail(request, kraj_id):
     kraj = get_object_or_404(Kraj, pk=kraj_id)
     return render(request, 'polls/detail.html', {'kraj': kraj})
+	
+def results(request, kraj_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % kraj_id)
+
+def vote(request, kraj_id):
+    return HttpResponse("You're voting on question %s." % kraj_id)
