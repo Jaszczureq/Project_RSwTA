b207b3ab6254350d1abbf1ad1f0e1c567204ef2d
Unknown <jakub.gosk358@gmail.com>
2018-05-28 20:58:05 +0200

    Added login feature

--- Project_RSwTA/urls.py
+++ Project_RSwTA/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import url
+from django.contrib import admin, auth
+from django.urls import include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    url('polls/', include('polls.urls')),
+    url('admin/', admin.site.urls),
+    url('accounts/', include('django.contrib.auth.urls')),
+	
 ]
