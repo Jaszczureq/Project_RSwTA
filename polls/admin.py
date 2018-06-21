40ac5b68e2e59f255a55bd645239e96ec07b6485
Jakub Gosk <kabanos358@gmail.com>
2018-06-21 08:02:59 +0200

    My last push before getting a grade

--- polls/admin.py
+++ polls/admin.py
@@ -24,7 +24,7 @@ class KandydatAdmin(admin.ModelAdmin):
 
 
 #admin.site.register(Kraj, KrajAdmin)
-#admin.site.register(Adres_zamieszkania, AdresAdmin)
+admin.site.register(Adres_zamieszkania, AdresAdmin)
 admin.site.register(Osoba)
 admin.site.register(Kryterium)
 admin.site.register(Wybor)
